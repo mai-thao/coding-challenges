1aa263594422d4a11970c586ed3019f5a89d389e
thao-mai <mthaominn@gmail.com>
2025-04-05 13:51:06 -0500

    Update names

--- RotateArray.java
+++ RotateArray.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * 
  * INPUT:
  * size = 3
- * img  = 
+ * arr  = 
  *    8 2 3
  *    2 9 1
  *    9 0 6
@@ -19,25 +19,25 @@ import java.util.Scanner;
  *    8 2 9
  */
 
-public class RotateImage {
-    public static void rotate_image(int size, int[][] img) {
+public class RotateArray {
+    public static void rotate_arr(int size, int[][] arr) {
         if (size == 0) {
             return;
         }
         // transpose
         for (int i = 0; i < size; i++) {
             for (int j = i; j < size; j++) {
-                int temp = img[i][j];
-                img[i][j] = img[j][i];
-                img[j][i] = temp;
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
         // swap each column
         for (int j = 0; j < size; j++) {
             for (int i = 0; i < size / 2; i++) {
-                int temp = img[i][j];
-                img[i][j] = img[size - i - 1][j];
-                img[size - i - 1][j] = temp;
+                int temp = arr[i][j];
+                arr[i][j] = arr[size - i - 1][j];
+                arr[size - i - 1][j] = temp;
             }
         }
     }
@@ -46,16 +46,16 @@ public class RotateImage {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         scanner.close();
-        int[][] img = new int[size][size];
+        int[][] arr = new int[size][size];
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                img[i][j] = scanner.nextInt();
+                arr[i][j] = scanner.nextInt();
             }
         }
-        rotate_image(size, img);
+        rotate_arr(size, arr);
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                System.out.print(img[i][j]);
+                System.out.print(arr[i][j]);
 
                 if (j < size - 1) {
                     System.out.print(" ");
