18434f0a07cfbff9f75a905f1d2753fe7919bc47
mai-thao <mthaominn@gmail.com>
2025-04-16 22:09:58 -0500

    Alternate solution to odd occurrence script

--- python/find-odd-occurrences.py
+++ python/find-odd-occurrences.py
@@ -1,4 +1,4 @@
-# Assume that there is ONLY ever one odd occurrence in the list
+# This 1st solution assumes there is ONLY one odd occurrence in the list
 def find_odd_occurrences(num_list):
     if not num_list:
         return None
@@ -23,4 +23,39 @@ print(result)
 # No odd occurrences
 list = [1,2,3,1,2,3,1,1]
 result = find_odd_occurrences(list)
+print(result)
+
+print("--------------------Using Dictionary--------------------")
+
+def find_odd_occurrences_using_dict(num_list):
+    num_count_dict = {}
+    result = []
+    
+    for num in num_list:
+        num_count_dict[num] = num_count_dict.get(num, 0) + 1
+
+    for num, count in num_count_dict.items():
+        if count % 2 == 1:
+            result.append(num)
+
+    return result
+
+# 1 odd occurrences
+list = [1,2,3,1,2,3,1]
+result = find_odd_occurrences_using_dict(list)
+print(result)
+
+# 2 odd occurrences
+list = [1,2,3,1,2,3,1,2]
+result = find_odd_occurrences_using_dict(list)
+print(result)
+
+# Empty list
+list = []
+result = find_odd_occurrences_using_dict(list)
+print(result)
+
+# No odd occurrences
+list = [1,2,3,1,2,3,1,1]
+result = find_odd_occurrences_using_dict(list)
 print(result)
