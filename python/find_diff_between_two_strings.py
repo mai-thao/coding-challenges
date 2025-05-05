67d23f1207f4a10f74891fa3d21a769b3f4dca02
mai-thao <mthaominn@gmail.com>
2025-05-04 19:22:22 -0500

    Add character diff script

---
+++ python/find_diff_between_two_strings.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+def unique_characters(str1, str2):
+    # Remove common whitespace characters from both strings
+    str1 = str1.replace(" ", "").replace("\t", "").replace("\n", "")
+    str2 = str2.replace(" ", "").replace("\t", "").replace("\n", "")
+
+    # Count the frequency of each character in both strings
+    counter1 = Counter(str1)
+    counter2 = Counter(str2)
+
+    # Store characters that are unique to each string
+    unique_chars = []
+
+    for char in counter1:
+        if char in counter2:
+            if counter1[char] > counter2[char]:
+                unique_chars.extend([char] * (counter1[char] - counter2[char]))
+        else:
+            unique_chars.extend([char] * counter1[char])
+
+    for char in counter2:
+        if char in counter1:
+            if counter2[char] > counter1[char]:
+                unique_chars.extend([char] * (counter2[char] - counter1[char]))
+        else:
+            unique_chars.extend([char] * counter2[char])
+
+    return ''.join(unique_chars)
