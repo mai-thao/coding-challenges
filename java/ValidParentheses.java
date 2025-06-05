23c316f3572757879a373d8555917fdb547ae4d4
mai-thao <mthaominn@gmail.com>
2025-06-05 17:01:24 -0500

    Add function comment and test restructure

--- java/ValidParentheses.java
+++ java/ValidParentheses.java
@@ -1,10 +1,32 @@
 import java.util.Stack;
 
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ *
+ * An input string is valid if:
+ * - Open brackets must be closed by the same type of brackets
+ * - Open brackets must be closed in the correct order
+ * - Every close bracket has a corresponding open bracket of the same type
+ *
+ * Examples:
+ * s = "()"
+ * Output: true
+ *
+ * s = "()[]{}"
+ * Output: true
+ *
+ * s = "(]"
+ * Output: false
+ *
+ * s = "([])"
+ * Output: true
+ *
+ * Time complexity: O(n) because for loop is traversing entire String
+ * Space complexity: O(n) because needed a stack where worst case it stores all open brackets
+ */
 public class ValidParentheses {
-    public static boolean isValid(String s) {
-        if (s.length() == 1) {
-            return false;
-        }
+    public static boolean isValidParentheses(String s) {
         Stack<Character> openBrackets = new Stack<>();
         for (char c: s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') { // Save open brackets to stack
@@ -27,14 +49,14 @@ public class ValidParentheses {
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("()")); // true
-        System.out.println(isValid("()[]{}")); // true
-        System.out.println(isValid("([])")); // true
-        System.out.println(isValid("(]")); // false
-        System.out.println(isValid("((")); // false
-        System.out.println(isValid("(")); // false
-        System.out.println(isValid("}")); // false
-        System.out.println(isValid("}}")); // false
+        System.out.println(isValidParentheses("()")); // true
+        System.out.println(isValidParentheses("()[]{}")); // true
+        System.out.println(isValidParentheses("([])")); // true
+        System.out.println(isValidParentheses("(]")); // false
+        System.out.println(isValidParentheses("((")); // false
+        System.out.println(isValidParentheses("(")); // false
+        System.out.println(isValidParentheses("}")); // false
+        System.out.println(isValidParentheses("}}")); // false
 
     }
 }
