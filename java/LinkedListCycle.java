2c709f4ed026891bc69aa67442df26d4af547354
mai-thao <mthaominn@gmail.com>
2025-06-12 14:24:35 -0500

    Add hasCycle solution

---
+++ java/LinkedListCycle.java
@@ -0,0 +1,34 @@
+public class LinkedListCycle {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(0);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2; // Cycle node
+        System.out.println(hasCycle(node1)); // true
+    }
+}
