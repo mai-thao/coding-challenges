132f3a54cc501f6de24c8a75b691eba7eed953a4
mai-thao <mthaominn@gmail.com>
2025-06-13 15:14:23 -0500

    Add class comment and solution using a HashSet

--- java/MissingNumber.java
+++ java/MissingNumber.java
@@ -1,5 +1,25 @@
+import java.util.HashSet;
+
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ *
+ * Examples:
+ * nums = [3,0,1]
+ * Output: 2 is the missing number
+ *
+ * nums = [0,1]
+ * Output: 2 is the missing number
+ *
+ * nums = [9,6,4,2,3,5,7,0,1]
+ * Output: 8 is the missing number
+ */
 public class MissingNumber {
-    public static int missingNumber(int[] nums) {
+    /**
+     * Time complexity: O(n) because traversing each int in the array
+     * Space complexity: O(1) because not using any extra dataset
+     */
+    public static int missingNumberSummation(int[] nums) {
         int n = nums.length;
         int expectedSum = 0;
         for (int i = 0; i <= n; i++) {
@@ -12,10 +32,34 @@ public class MissingNumber {
         return expectedSum - sum;
     }
 
+    /**
+     * Time complexity: O(n) because traversing each int in the array
+     * Space complexity: O(n) because using HashSet dataset to store each num
+     */
+    public static int missingNumberHashSet(int[] nums) {
+        HashSet<Integer> existingNums = new HashSet<>();
+        for (int num: nums) {
+            existingNums.add(num);
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            if (!existingNums.contains(i)) {
+                return i;
+            }
+        }
+        throw new IllegalStateException("There must be a solution!");
+    }
+
     public static void main(String[] args) {
-        System.out.println(missingNumber(new int[]{3,0,1}));
-        System.out.println(missingNumber(new int[]{0,1}));
-        System.out.println(missingNumber(new int[]{0}));
-        System.out.println(missingNumber(new int[]{9,6,4,2,3,5,7,0,1}));
+        System.out.println(missingNumberSummation(new int[]{3,0,1})); // 2
+        System.out.println(missingNumberSummation(new int[]{0,1})); // 2
+        System.out.println(missingNumberSummation(new int[]{0})); // 1
+        System.out.println(missingNumberSummation(new int[]{9,6,4,2,3,5,7,0,1})); // 8
+
+        System.out.println();
+
+        System.out.println(missingNumberHashSet(new int[]{3,0,1}));
+        System.out.println(missingNumberHashSet(new int[]{0,1}));
+        System.out.println(missingNumberHashSet(new int[]{0}));
+        System.out.println(missingNumberHashSet(new int[]{9,6,4,2,3,5,7,0,1}));
     }
 }
