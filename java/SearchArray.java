ef6d6edd621db8a860bc8cf1f4ed70ee8025286e
mai-thao <mthaominn@gmail.com>
2025-06-23 14:39:34 -0500

    Add binarySearch method

--- java/SearchArray.java
+++ java/SearchArray.java
@@ -27,6 +27,22 @@ public class SearchArray {
         return -1;
     }
 
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println(linearSearch(new int[]{-1,0,3,5,9,12}, 9)); // 4
         System.out.println(linearSearch(new int[]{-1,0,3,5,9,12}, 2)); // -1
