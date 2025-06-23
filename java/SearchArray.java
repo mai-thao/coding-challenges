cfc1cf5d1773f4a69ad9d07aa86cdb642646fd12
mai-thao <mthaominn@gmail.com>
2025-06-23 15:11:11 -0500

    Add recursiveSearch solution

--- java/SearchArray.java
+++ java/SearchArray.java
@@ -47,15 +47,48 @@ public class SearchArray {
         return -1;
     }
 
+    /**
+     * Time complexity: O(log n) because halving the array each time
+     * Space complexity: O(log n) because using the recursive call stack with each halving step
+     */
+    public static int recursiveSearch(int[] nums, int left, int right, int target) {
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return recursiveSearch(nums, left, mid - 1, target);
+        } else {
+            return recursiveSearch(nums, mid + 1, right, target);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(linearSearch(new int[]{-1,0,3,5,9,12}, 9)); // 4
-        System.out.println(linearSearch(new int[]{-1,0,3,5,9,12}, 2)); // -1
-        System.out.println(linearSearch(new int[]{5}, 5)); // 0
+        int[] arr1 = new int[]{-1,0,3,5,9,12};
+        int[] arr2 = new int[]{5};
+        int[] arr3 = new int[]{1,3,5,6};
+
+        System.out.println(linearSearch(arr1, 9)); // 4
+        System.out.println(linearSearch(arr1, 2)); // -1
+        System.out.println(linearSearch(arr2, 5)); // 0
+        System.out.println(linearSearch(arr3, 7)); // -1
 
         System.out.println();
 
-        System.out.println(binarySearch(new int[]{-1,0,3,5,9,12}, 9)); // 4
-        System.out.println(binarySearch(new int[]{-1,0,3,5,9,12}, 2)); // -1
-        System.out.println(binarySearch(new int[]{5}, 5)); // 0
+        System.out.println(binarySearch(arr1, 9)); // 4
+        System.out.println(binarySearch(arr1, 2)); // -1
+        System.out.println(binarySearch(arr2, 5)); // 0
+        System.out.println(binarySearch(arr3, 7)); // -1
+
+        System.out.println();
+
+        System.out.println(recursiveSearch(arr1, 0, arr1.length-1, 9)); // 4
+        System.out.println(recursiveSearch(arr1, 0, arr1.length-1, 2)); // -1
+        System.out.println(recursiveSearch(arr2, 0, arr2.length-1,  5)); // 0
+        System.out.println(recursiveSearch(arr3, 0, arr3.length-1, 7)); // -1
     }
 }
