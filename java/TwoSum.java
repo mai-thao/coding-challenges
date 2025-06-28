035f0aa82fbc73b7f5564587193b84450e217103
mai-thao <mthaominn@gmail.com>
2025-06-28 08:34:41 -0500

    Add expected outputs to comments

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -51,14 +51,14 @@ class TwoSum {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{2,7,11,15}, 9)));
-        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{3,2,4}, 6)));
-        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{3,3}, 6)));
-        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{3,2,3}, 6)));
+        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{2,7,11,15}, 9))); // [0,1]
+        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{3,2,4}, 6))); // [1,2]
+        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{3,3}, 6))); // [0,1]
+        System.out.println(Arrays.toString(twoSumBruteForce(new int[]{3,2,3}, 6))); // [0,2]
         System.out.println("-------");
-        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9)));
-        System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6)));
-        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6)));
-        System.out.println(Arrays.toString(twoSum(new int[]{3,2,3}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9))); // [0,1]
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6))); // [1,2]
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6))); // [0,1]
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,3}, 6))); // [0,2]
     }
 }
