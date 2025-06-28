4e8cce936e3e00a09de3ab3e0fc1e0fa310afb9c
mai-thao <mthaominn@gmail.com>
2025-06-28 08:34:56 -0500

    Add more tests and expected outputs in comments

--- java/ReverseLinkedList.java
+++ java/ReverseLinkedList.java
@@ -41,15 +41,31 @@ public class ReverseLinkedList {
         ListNode node3 = new ListNode(3, node2);
         ListNode node4 = new ListNode(2, node3);
         ListNode node5 = new ListNode(1, node4);
-        printNodes(node5);
+        printNodes(node5); // 1->2->3->4->5
         ListNode reversedNodes = reverseList(node5);
-        printNodes(reversedNodes);
+        printNodes(reversedNodes); // 5->4->3->2->1
 
         // Test 2
         ListNode node6 = new ListNode(2);
         ListNode node7 = new ListNode(1, node6);
-        printNodes(node7);
+        printNodes(node7); // 1->2
         reversedNodes = reverseList(node7);
-        printNodes(reversedNodes);
+        printNodes(reversedNodes); // 2->1
+
+        // Test 3
+        ListNode node8 = new ListNode(11);
+        printNodes(node8); // 11
+        reversedNodes = reverseList(node8);
+        printNodes(reversedNodes); // 11
+
+        // Test 4
+        ListNode node9 = new ListNode(8);
+        ListNode node10 = new ListNode(3, node9);
+        ListNode node11 = new ListNode(5, node10);
+        ListNode node12 = new ListNode(12, node11);
+        ListNode node13 = new ListNode(1, node12);
+        printNodes(node13); // 1->12->5->3->8
+        reversedNodes = reverseList(node13);
+        printNodes(reversedNodes); // 8->3->5->12->1
     }
 }
