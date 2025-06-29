fe1da8234dedf64a90280374a4d9c77fecaac9e1
mai-thao <mthaominn@gmail.com>
2025-06-29 15:13:01 -0500

    Add test cases into main

--- java/FindMiddleNode.java
+++ java/FindMiddleNode.java
@@ -18,6 +18,40 @@ public class FindMiddleNode {
         return slow;
     }
 
+    // Helper fxn to print the values of each node in order
+    public static void printNodes(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] main) {
+        ListNode node1 = new ListNode(5);
+        ListNode node2 = new ListNode(4, node1);
+        ListNode node3 = new ListNode(3, node2);
+        ListNode node4 = new ListNode(2, node3);
+        ListNode node5 = new ListNode(1, node4);
+        printNodes(node5);
+        printNodes(middleNode(node5));
+
+        System.out.println();
+
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(5, node6);
+        ListNode node8 = new ListNode(4, node7);
+        ListNode node9 = new ListNode(3, node8);
+        ListNode node10 = new ListNode(2, node9);
+        ListNode node11 = new ListNode(1, node10);
+        printNodes(node11);
+        printNodes(middleNode(node11));
+
+        System.out.println();
+
+        ListNode node12 = new ListNode(1);
+        printNodes(node12);
+        printNodes(middleNode(node12));
     }
 }
