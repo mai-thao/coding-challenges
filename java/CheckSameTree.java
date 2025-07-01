4c97d0bffb68698e4091f361d78dbf2759f0710e
mai-thao <mthaominn@gmail.com>
2025-07-01 12:05:37 -0500

    Add 3 test cases

--- java/CheckSameTree.java
+++ java/CheckSameTree.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class CheckSameTree {
-     public class TreeNode {
+     public static class TreeNode {
          int val;
          TreeNode left;
          TreeNode right;
@@ -15,7 +15,7 @@ public class CheckSameTree {
          }
      }
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
         Queue<TreeNode> queue1 = new LinkedList<>();
         Queue<TreeNode> queue2 = new LinkedList<>();
 
@@ -41,5 +41,35 @@ public class CheckSameTree {
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1PNode2 = new TreeNode(2);
+        TreeNode t1PNode3 = new TreeNode(3);
+        TreeNode t1PNode1 = new TreeNode(1, t1PNode2, t1PNode3); // [1,2,3]
+
+        TreeNode t1QNode2 = new TreeNode(2);
+        TreeNode t1QNode3 = new TreeNode(3);
+        TreeNode t1QNode1 = new TreeNode(1, t1QNode2, t1QNode3); // [1,2,3]
+
+        System.out.println("Test 1: " + isSameTree(t1PNode1, t1QNode1)); // true
+
+        TreeNode t2PNode2 = new TreeNode(2);
+        TreeNode t2PNode1 = new TreeNode(1, t2PNode2, null); // [1,2]
+
+        TreeNode t2QNode2 = new TreeNode(2);
+        TreeNode t2QNode1 = new TreeNode(1, null, t2QNode2); // [1,null,2]
+
+        System.out.println("Test 2: " + isSameTree(t2PNode1, t2QNode1)); // false
+
+        TreeNode t3PNode2 = new TreeNode(2);
+        TreeNode t3PNode3 = new TreeNode(1);
+        TreeNode t3PNode1 = new TreeNode(1, t3PNode2, t3PNode3); // [1,2,1]
+
+        TreeNode t3QNode2 = new TreeNode(1);
+        TreeNode t3QNode3 = new TreeNode(2);
+        TreeNode t3QNode1 = new TreeNode(1, t3QNode2, t3QNode3); // [1,1,2]
+
+        System.out.println("Test 2: " + isSameTree(t3PNode1, t3QNode1)); // false
     }
 }
