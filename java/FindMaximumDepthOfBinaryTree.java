c9c4c8de94f58b18efa2e95f00ec98cad01c718b
mai-thao <mthaominn@gmail.com>
2025-07-02 22:25:34 -0500

    Add recursive solution too

--- java/FindMaximumDepthOfBinaryTree.java
+++ java/FindMaximumDepthOfBinaryTree.java
@@ -1,6 +1,10 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Given the root of a binary tree, return its maximum depth. A binary tree's maximum depth is the
+ * number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
 public class FindMaximumDepthOfBinaryTree {
     public static class TreeNode {
          int val;
@@ -38,6 +42,11 @@ public class FindMaximumDepthOfBinaryTree {
         return depth;
     }
 
+    public static int maxDepthRecursive(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepthRecursive(root.left),  maxDepthRecursive(root.right));
+    }
+
     public static void main(String[] args) {
         TreeNode t1Node15 = new TreeNode(15);
         TreeNode t1Node7 = new TreeNode(7);
@@ -45,14 +54,24 @@ public class FindMaximumDepthOfBinaryTree {
         TreeNode t1Node9 = new TreeNode(9);
         TreeNode t1Node3 = new TreeNode(3, t1Node9, t1Node20);
         System.out.println(maxDepth(t1Node3)); // [3,9,20,null,null,15,7] -> 3
+        System.out.println(maxDepthRecursive(t1Node3));
+
+        System.out.println("-----");
 
         TreeNode t2Node2 = new TreeNode(2);
         TreeNode t2Node1 = new TreeNode(1, null, t2Node2);
         System.out.println(maxDepth(t2Node1)); // [1,null,2] -> 2
+        System.out.println(maxDepthRecursive(t2Node1));
+
+        System.out.println("-----");
 
         System.out.println(maxDepth(null)); // [] -> 0
+        System.out.println(maxDepthRecursive(null));
+
+        System.out.println("-----");
 
         TreeNode t4Node5 = new TreeNode(5);
         System.out.println(maxDepth(t4Node5)); // [5] -> 1
+        System.out.println(maxDepthRecursive(t4Node5));
     }
 }
