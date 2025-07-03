7efef6abfcbc9144271caf83d9cb538573dfa789
mai-thao <mthaominn@gmail.com>
2025-07-03 15:55:07 -0500

    Add more test cases and null unskewed side

--- java/InvertBinaryTree.java
+++ java/InvertBinaryTree.java
@@ -1,6 +1,9 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Given the root of a binary tree, invert the tree, and return its root.
+ */
 public class InvertBinaryTree {
     public static class TreeNode {
         int val;
@@ -33,9 +36,11 @@ public class InvertBinaryTree {
                     queue.add(popped.right);
                 } else if (popped.left != null) {
                     popped.right = popped.left;
+                    popped.left = null;
                     queue.add(popped.right);
                 } else if (popped.right != null) {
                     popped.left = popped.right;
+                    popped.right = null;
                     queue.add(popped.left);
                 }
             }
@@ -44,8 +49,11 @@ public class InvertBinaryTree {
     }
 
     public static void printTreeNode(TreeNode root) {
+        if (root == null) System.out.print("Empty");
+
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+
         while (!queue.isEmpty()) {
             int levelSize = queue.size();
             for (int i = 0; i < levelSize; i++) {
@@ -72,5 +80,22 @@ public class InvertBinaryTree {
         printTreeNode(t1root);
         System.out.print(" -> ");
         printTreeNode(invertTree(t1root)); // [4,2,7,1,3,6,9] -> [4,7,2,9,6,3,1]
+
+        System.out.println();
+
+        TreeNode t2Node1 = new TreeNode(1);
+        TreeNode t2Node3 = new TreeNode(3);
+        TreeNode t2root = new TreeNode(2, t2Node1, t2Node3);
+        printTreeNode(t2root);
+        System.out.print(" -> ");
+        printTreeNode(invertTree(t2root)); // [2,1,3] -> [2,3,1]
+
+        System.out.println();
+
+        TreeNode t3Node2 = new TreeNode(2);
+        TreeNode t3root = new TreeNode(1, t3Node2, null);
+        printTreeNode(t3root);
+        System.out.print(" -> ");
+        printTreeNode(invertTree(t3root)); // [1,2] -> [1,null,2] ** The null is not printed
     }
 }
