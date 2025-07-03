9b280624511e753e244ea2aa70aec93ec7c8ff08
mai-thao <mthaominn@gmail.com>
2025-07-02 21:52:30 -0500

    Add test cases

--- java/FindMaximumDepthOfBinaryTree.java
+++ java/FindMaximumDepthOfBinaryTree.java
@@ -1,8 +1,10 @@
+import com.sun.source.tree.Tree;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class FindMaximumDepthOfBinaryTree {
-    public class TreeNode {
+    public static class TreeNode {
          int val;
          TreeNode left;
          TreeNode right;
@@ -15,7 +17,7 @@ public class FindMaximumDepthOfBinaryTree {
          }
     }
 
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(TreeNode root) {
         int depth = 0;
         if (root == null) return depth;
 
@@ -35,5 +37,24 @@ public class FindMaximumDepthOfBinaryTree {
             depth++;
         }
         return depth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1Node15 = new TreeNode(15);
+        TreeNode t1Node7 = new TreeNode(7);
+        TreeNode t1Node20 = new TreeNode(20, t1Node15, t1Node7);
+        TreeNode t1Node9 = new TreeNode(9);
+        TreeNode t1Node3 = new TreeNode(3, t1Node9, t1Node20);
+        System.out.println(maxDepth(t1Node3)); // [3,9,20,null,null,15,7] -> 3
+
+        TreeNode t2Node2 = new TreeNode(2);
+        TreeNode t2Node1 = new TreeNode(1, null, t2Node2);
+        System.out.println(maxDepth(t2Node1)); // [1,null,2] -> 2
+
+        System.out.println(maxDepth(null)); // [] -> 0
+
+        TreeNode t4Node5 = new TreeNode(5);
+        System.out.println(maxDepth(t4Node5)); // [5] -> 1
+
     }
 }
