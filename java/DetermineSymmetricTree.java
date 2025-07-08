21ee1e7f8710747686848d511c3429e1c2fff937
mai-thao <mthaominn@gmail.com>
2025-07-07 22:04:49 -0500

    Add final iterative BFS approach and complexity explainations

--- java/DetermineSymmetricTree.java
+++ java/DetermineSymmetricTree.java
@@ -18,7 +18,11 @@ public class DetermineSymmetricTree {
         }
     }
 
-    public boolean isSymmetric(TreeNode root) {
+    /**
+     * Time complexity: O(n) because traversing every node in the tree
+     * Space complexity: O(n) because storing every node in both queues
+     */
+    public static boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
         if (root.left == null && root.right == null) return true;
 
@@ -32,9 +36,11 @@ public class DetermineSymmetricTree {
             TreeNode leftCurrNode = leftQueue.poll();
             TreeNode rightCurrNode = rightQueue.poll();
 
+            if (leftCurrNode == null && rightCurrNode == null) continue; // `continue` keyword skips current iteration
             if (leftCurrNode == null || rightCurrNode == null) return false;
             if (leftCurrNode.val != rightCurrNode.val) return false;
 
+            // Order matters for symmetry so pay special attention!
             leftQueue.add(leftCurrNode.left);
             leftQueue.add(leftCurrNode.right);
             rightQueue.add(rightCurrNode.right);
@@ -43,6 +49,10 @@ public class DetermineSymmetricTree {
         return true;
     }
 
+    /**
+     * Time complexity: O(n) because traversing every node in the tree
+     * Space complexity: O(n/2) -> O(n) dropping constants where in perfect BT at leaf level, all nodes are stored into queues
+     */
     public static boolean isSymmetricRecursive(TreeNode root) {
         return isMirror(root, root);
     }
@@ -62,15 +72,22 @@ public class DetermineSymmetricTree {
         TreeNode t1RightNode3 = new TreeNode(3);
         TreeNode t1RightNode2 = new TreeNode(2, t1RightNode4, t1RightNode3);
         TreeNode t1Root = new TreeNode(1, t1LeftNode2, t1RightNode2);
-        System.out.println(isSymmetricRecursive(t1Root)); // [1,2,2,3,4,4,3] -> true
+        System.out.println(isSymmetric(t1Root)); // [1,2,2,3,4,4,3] -> true
+        System.out.println(isSymmetricRecursive(t1Root));
+
+        System.out.println();
 
         TreeNode t2LeftNode3 = new TreeNode(3);
         TreeNode t2LeftNode2 = new TreeNode(2, null, t2LeftNode3);
         TreeNode t2RightNode3 = new TreeNode(3);
         TreeNode t2RightNode2 = new TreeNode(2, null, t2RightNode3);
         TreeNode t2Root = new TreeNode(1, t2LeftNode2, t2RightNode2);
-        System.out.println(isSymmetricRecursive(t2Root)); // [1,2,2,null,3,null,3] -> false
+        System.out.println(isSymmetric(t2Root)); // [1,2,2,null,3,null,3] -> false
+        System.out.println(isSymmetricRecursive(t2Root));
 
-        System.out.println(isSymmetricRecursive(new TreeNode(5))); // [5] -> true
+        System.out.println();
+
+        System.out.println(isSymmetric(new TreeNode(5))); // [5] -> true
+        System.out.println(isSymmetricRecursive(new TreeNode(5)));
     }
 }
