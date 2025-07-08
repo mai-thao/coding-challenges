7d0304329c4f31ec84372d081fbc137f451bba8d
mai-thao <mthaominn@gmail.com>
2025-07-07 21:25:29 -0500

    Add DetermineSymmetricTree recursive DFS solution

---
+++ java/DetermineSymmetricTree.java
@@ -0,0 +1,48 @@
+/**
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e. symmetric around its center).
+ */
+public class DetermineSymmetricTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isSymmetricRecursive(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    // Helper fxn for recursive approach
+    private static boolean isMirror(TreeNode leftRoot, TreeNode rightRoot) {
+        if (leftRoot == null && rightRoot == null) return true;
+        if (leftRoot == null || rightRoot == null) return false;
+        return (leftRoot.val == rightRoot.val) && isMirror(leftRoot.left, rightRoot.right) && isMirror(leftRoot.right, rightRoot.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1LeftNode3 = new TreeNode(3);
+        TreeNode t1LeftNode4 = new TreeNode(4);
+        TreeNode t1LeftNode2 = new TreeNode(2, t1LeftNode3, t1LeftNode4);
+        TreeNode t1RightNode4 = new TreeNode(4);
+        TreeNode t1RightNode3 = new TreeNode(3);
+        TreeNode t1RightNode2 = new TreeNode(2, t1RightNode4, t1RightNode3);
+        TreeNode t1Root = new TreeNode(1, t1LeftNode2, t1RightNode2);
+        System.out.println(isSymmetricRecursive(t1Root)); // [1,2,2,3,4,4,3] -> true
+
+        TreeNode t2LeftNode3 = new TreeNode(3);
+        TreeNode t2LeftNode2 = new TreeNode(2, null, t2LeftNode3);
+        TreeNode t2RightNode3 = new TreeNode(3);
+        TreeNode t2RightNode2 = new TreeNode(2, null, t2RightNode3);
+        TreeNode t2Root = new TreeNode(1, t2LeftNode2, t2RightNode2);
+        System.out.println(isSymmetricRecursive(t2Root)); // [1,2,2,null,3,null,3] -> false
+
+        System.out.println(isSymmetricRecursive(new TreeNode(5))); // [5] -> true
+    }
+}
