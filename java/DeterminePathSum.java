06a7ae5e86b49d5629697f6eda9e6fceed714cbc
mai-thao <mthaominn@gmail.com>
2025-07-07 19:23:41 -0500

    Add test cases for iterative approach

--- java/DeterminePathSum.java
+++ java/DeterminePathSum.java
@@ -6,7 +6,7 @@ import java.util.Stack;
  * children.
  */
 public class DeterminePathSum {
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -19,7 +19,7 @@ public class DeterminePathSum {
         }
     }
 
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
 
         Stack<TreeNode> nodeStack = new Stack<>();
@@ -43,5 +43,35 @@ public class DeterminePathSum {
             }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasPathSum(null, 0)); // [], 0 -> false
+        System.out.println(hasPathSum(new TreeNode(1), 1)); // [1], 1 = true
+        System.out.println(hasPathSum(new TreeNode(1), 2)); // [1], 2 = false
+
+        TreeNode t1Node7 = new TreeNode(7);
+        TreeNode t1Node2 = new TreeNode(2);
+        TreeNode t1Node11 = new TreeNode(11, t1Node7, t1Node2);
+        TreeNode t1NodeLeft4 = new TreeNode(4, t1Node11, null);
+        TreeNode t1Node1 = new TreeNode(1);
+        TreeNode t1Node13 = new TreeNode(13);
+        TreeNode t1NodeRight4 = new TreeNode(4, null, t1Node1);
+        TreeNode t1Node8 = new TreeNode(8, t1Node13, t1NodeRight4);
+        TreeNode t1Root = new TreeNode(5, t1NodeLeft4, t1Node8);
+        System.out.println(hasPathSum(t1Root, 22)); // [5,4,8,11,null,13,4,7,2,null,null,null,1], 22 -> true
+
+        TreeNode t2Node2 = new TreeNode(2);
+        TreeNode t2Node3 = new TreeNode(3);
+        TreeNode t2Root = new TreeNode(1, t2Node2, t2Node3);
+        System.out.println(hasPathSum(t2Root, 5)); // [1,2,3], 5 -> false
+
+        TreeNode t3Node1 = new TreeNode(1);
+        TreeNode t3Node3 = new TreeNode(3);
+        TreeNode t3NodeLeftNeg2 = new TreeNode(-2, t3Node1, t3Node3);
+        TreeNode t3NodeRightNeg2 = new TreeNode(-2);
+        TreeNode t3NodeNeg3 = new TreeNode(-3, t3NodeLeftNeg2, null);
+        TreeNode t3Root = new TreeNode(1, t3NodeRightNeg2, t3NodeNeg3);
+        System.out.println(hasPathSum(t3Root, 3)); // [1,-2,-3,1,3,-2], 3 -> false
     }
 }
