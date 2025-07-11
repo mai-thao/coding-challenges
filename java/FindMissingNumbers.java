5770d17ebcf811213b99b6e1b67be26397594879
mai-thao <mthaominn@gmail.com>
2025-07-11 11:27:11 -0500

    Add FindMissingNumbers solution

---
+++ java/FindMissingNumbers.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Given an array `nums` of n integers where `nums[i]` is in the range [1, n], return an array
+ * of all the integers in the range [1, n] that do not appear in `nums`.
+ */
+public class FindMissingNumbers {
+    /**
+     * Time complexity: O(n) because traversing the entire nums array
+     * Space complexity: O(n) because needed HashSet where in worst case all nums are unique so they're stored into it
+     */
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        HashSet<Integer> uniqueNums = new HashSet<>();
+        for (int num: nums) {
+            uniqueNums.add(num);
+        }
+        ArrayList<Integer> missingNums = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!uniqueNums.contains(i)) {
+                missingNums.add(i);
+            }
+        }
+        return missingNums;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1})); // [5,6]
+        System.out.println(findDisappearedNumbers(new int[]{1,1})); // [2]
+        System.out.println(findDisappearedNumbers(new int[]{2})); // [2]
+        System.out.println(findDisappearedNumbers(new int[]{1})); // []
+    }
+}
