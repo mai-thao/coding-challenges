dd6fb58ab240fc9fe463ef09e2277f18fdd9dc29
mai-thao <mthaominn@gmail.com>
2025-09-03 15:51:37 -0500

    Fix typo in problem description

--- java/BasicArrayTransformation.java
+++ java/BasicArrayTransformation.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 /**
- * Given an array, return a new array of trhe same length by applying the following transformation:
+ * Given an array `a`, return a new array `b` of the same length by applying the following transformations:
  * - For each i from 0 to a.length - 1 inclusive, b[i] = a[i-1] + a[i] + a[i+1]
  * - If an element in the sum a[i-1] + a[i] + a[i+1] does not exist, use 0 in its place
  * - For instance, b[0] = 0 + a[0] + a[1]
