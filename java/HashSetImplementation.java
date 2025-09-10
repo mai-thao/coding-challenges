5464b34ccb6c711b6c2abb13035063301b3866d4
mai-thao <mthaominn@gmail.com>
2025-09-09 22:19:46 -0500

    Rename and add simple test run

--- java/HashSetImplementation.java
+++ java/HashSetImplementation.java
@@ -3,15 +3,15 @@ import java.util.LinkedList;
 /**
  * Design a HashSet without using any built-in hash table libraries.
  *
- * Implement MyHashSet class:
+ * Implement HashSetImplementation class:
  * - `void add(key)` Inserts the value key into the HashSet.
  * - `bool contains(key)` Returns whether the value key exists in the HashSet or not.
  * - `void remove(key)` Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
  */
-public class MyHashSet {
-    class HashSet {
+public class HashSetImplementation {
+    static class MyHashSet {
         private final int BUCKET_SIZE = 10000;
-        private LinkedList<Integer>[] bucket;
+        private final LinkedList<Integer>[] bucket;
 
         public MyHashSet() {
             bucket = new LinkedList[BUCKET_SIZE];
@@ -27,7 +27,6 @@ public class MyHashSet {
             }
         }
 
-        // reminder: 2 flavors of LinkedList.remove()
         public void remove(int key) {
             int bucketIdx = key % BUCKET_SIZE;
             if (bucket[bucketIdx].contains(key)) {
@@ -39,5 +38,17 @@ public class MyHashSet {
             int bucketIdx = key % BUCKET_SIZE;
             return bucket[bucketIdx].contains(key);
         }
+    }
+
+    public static void main(String[] args) {
+        MyHashSet myHashSet = new MyHashSet();
+        myHashSet.add(1);                           // set = [1]
+        myHashSet.add(2);                           // set = [1, 2]
+        System.out.println(myHashSet.contains(1));  // return true
+        System.out.println(myHashSet.contains(3));  // return false (not found)
+        myHashSet.add(2);                           // set = [1, 2]
+        System.out.println(myHashSet.contains(2));  // return true
+        myHashSet.remove(2);                   // set = [1]
+        System.out.println(myHashSet.contains(2));  // return false (already removed)
     }
 }
