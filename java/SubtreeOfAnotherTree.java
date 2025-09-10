b77a9afdbeacfc1e4f7e9908725e2be36b044f42
mai-thao <mthaominn@gmail.com>
2025-09-09 22:35:01 -0500

    Add tests and use helper buildTree fxn

--- java/SubtreeOfAnotherTree.java
+++ java/SubtreeOfAnotherTree.java
@@ -1,7 +1,10 @@
 import configs.Config.*;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import static configs.Config.buildTree;
 
 /**
  * Given the roots of two binary trees `root` and `subRoot`, return true if there is a subtree of `root` with `subRoot` and false otherwise.
@@ -13,7 +16,7 @@ public class SubtreeOfAnotherTree {
      * Time complexity: O(n*m) because traversing all nodes in both `root` and `subRoot` trees
      * Space complexity: O(max(w,h)) because largest value of width (queue stores all leaf nodes in perfectly balanced BT) or height (call stack in skewed tree)
      */
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
         Queue<TreeNode> rQueue = new LinkedList<>();
         rQueue.add(root);
 
@@ -38,7 +41,7 @@ public class SubtreeOfAnotherTree {
      * Time complexity: O(n*m) because traversing all nodes in both `root` and `subRoot` trees
      * Space complexity: O(h) because of the height of call stack in skewed tree (aka O(n) too)
      */
-    public boolean isSubtreeRecursive(TreeNode root, TreeNode subRoot) {
+    public static boolean isSubtreeRecursive(TreeNode root, TreeNode subRoot) {
         if (root == null) return false;
         if (isSame(root, subRoot)) return true;
 
@@ -46,11 +49,23 @@ public class SubtreeOfAnotherTree {
     }
 
     // Helper function to check equality of two trees: same structure and same values
-    private boolean isSame(TreeNode root, TreeNode subRoot) {
+    private static boolean isSame(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) return true;
         if (subRoot == null || root == null) return false;
         if (root.val != subRoot.val) return false;
 
         return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode p1Root = buildTree(Arrays.asList(3,4,5,1,2));
+        TreeNode p1SubRoot = buildTree(Arrays.asList(4,1,2));
+        System.out.println(isSubtree(p1Root, p1SubRoot)); // true
+        System.out.println(isSubtreeRecursive(p1Root, p1SubRoot)); // true
+
+        TreeNode p2Root = buildTree(Arrays.asList(3,4,5,1,2,null,null,null,null,0));
+        TreeNode p2SubRoot = buildTree(Arrays.asList(4,1,2));
+        System.out.println(isSubtree(p2Root, p2SubRoot)); // false
+        System.out.println(isSubtreeRecursive(p2Root, p2SubRoot)); // false
     }
 }
