5fb27b9d0c8467fa5fae8d0ea5ab8df51bed171e
mai-thao <mthaominn@gmail.com>
2025-09-19 12:02:08 -0500

    Add plus one solution arithmetically

--- java/PlusOne.java
+++ java/PlusOne.java
@@ -28,6 +28,26 @@ public class PlusOne {
         return resultDigits;
     }
 
+    public static int[] plusOneArithmetic(int[] digits) {
+        int lastIdx = digits.length - 1;
+        if (digits[lastIdx] < 9) {
+            digits[lastIdx] = digits[lastIdx] + 1;
+        } else {
+            while (lastIdx >= 0 && digits[lastIdx] == 9) {
+                digits[lastIdx] = 0;
+                lastIdx--;
+            }
+            if (lastIdx >= 0) {
+                digits[lastIdx] = digits[lastIdx] + 1;
+            } else {
+                int[] newDigits = new int[digits.length + 1];
+                newDigits[0] = 1;
+                return newDigits;
+            }
+        }
+        return digits;
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(plusOne(new int[]{1, 2, 3}))); // [1,2,4]
         System.out.println(Arrays.toString(plusOne(new int[]{4,3,2,1}))); // [4,3,2,2]
@@ -35,5 +55,14 @@ public class PlusOne {
         System.out.println(Arrays.toString(plusOne(new int[]{999999}))); // [1,0,0,0,0,0,0]
         System.out.println(Arrays.toString(plusOne(new int[]{9,8,7,6,5,4,3,2,1,0}))); // [9,8,7,6,5,4,3,2,1,1]
         System.out.println(Arrays.toString(plusOne(new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1}))); // [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,2]
+
+        System.out.println();
+
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{1, 2, 3}))); // [1,2,4]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{4,3,2,1}))); // [4,3,2,2]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{9}))); // [1,0]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{999999}))); // [1,0,0,0,0,0,0]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{9,8,7,6,5,4,3,2,1,0}))); // [9,8,7,6,5,4,3,2,1,1]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1}))); // [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,2]
     }
 }
