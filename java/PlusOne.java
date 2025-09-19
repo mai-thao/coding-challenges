60742c0efbacb53b2233d165208dde17ff2e3da3
mai-thao <mthaominn@gmail.com>
2025-09-19 12:05:48 -0500

    Fix incorrect test num bigger than 9 and override old arr

--- java/PlusOne.java
+++ java/PlusOne.java
@@ -40,9 +40,8 @@ public class PlusOne {
             if (lastIdx >= 0) {
                 digits[lastIdx] = digits[lastIdx] + 1;
             } else {
-                int[] newDigits = new int[digits.length + 1];
-                newDigits[0] = 1;
-                return newDigits;
+                digits = new int[digits.length + 1];
+                digits[0] = 1;
             }
         }
         return digits;
@@ -52,7 +51,8 @@ public class PlusOne {
         System.out.println(Arrays.toString(plusOne(new int[]{1, 2, 3}))); // [1,2,4]
         System.out.println(Arrays.toString(plusOne(new int[]{4,3,2,1}))); // [4,3,2,2]
         System.out.println(Arrays.toString(plusOne(new int[]{9}))); // [1,0]
-        System.out.println(Arrays.toString(plusOne(new int[]{999999}))); // [1,0,0,0,0,0,0]
+        System.out.println(Arrays.toString(plusOne(new int[]{8,9,9}))); // [9,0,0]
+        System.out.println(Arrays.toString(plusOne(new int[]{9,9,9,9,9,9}))); // [1,0,0,0,0,0,0]
         System.out.println(Arrays.toString(plusOne(new int[]{9,8,7,6,5,4,3,2,1,0}))); // [9,8,7,6,5,4,3,2,1,1]
         System.out.println(Arrays.toString(plusOne(new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1}))); // [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,2]
 
@@ -61,7 +61,8 @@ public class PlusOne {
         System.out.println(Arrays.toString(plusOneArithmetic(new int[]{1, 2, 3}))); // [1,2,4]
         System.out.println(Arrays.toString(plusOneArithmetic(new int[]{4,3,2,1}))); // [4,3,2,2]
         System.out.println(Arrays.toString(plusOneArithmetic(new int[]{9}))); // [1,0]
-        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{999999}))); // [1,0,0,0,0,0,0]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{8,9,9}))); // [9,0,0]
+        System.out.println(Arrays.toString(plusOneArithmetic(new int[]{9,9,9,9,9,9}))); // [1,0,0,0,0,0,0]
         System.out.println(Arrays.toString(plusOneArithmetic(new int[]{9,8,7,6,5,4,3,2,1,0}))); // [9,8,7,6,5,4,3,2,1,1]
         System.out.println(Arrays.toString(plusOneArithmetic(new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1}))); // [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,2]
     }
