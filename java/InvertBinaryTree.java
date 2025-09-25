fc56a637d612a93ac1636576f693cf127ff53cb1
mai-thao <mthaominn@gmail.com>
2025-09-24 22:47:20 -0500

    Use Config helper buildTree and print methods

--- java/InvertBinaryTree.java
+++ java/InvertBinaryTree.java
@@ -1,7 +1,11 @@
 import configs.Config.*;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import static configs.Config.buildTree;
+import static configs.Config.printLevelOrder;
 
 /**
  * Given the root of a binary tree, invert the tree, and return its root.
@@ -59,62 +63,27 @@ public class InvertBinaryTree {
         return root;
     }
 
-    public static void printTreeNode(TreeNode root) {
-        if (root == null) {
-            System.out.print("Empty");
-            return; // Break out of void method
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode popped = queue.poll();
-                System.out.print(popped.val + ",");
-                if (popped.left != null) {
-                    queue.add(popped.left);
-                }
-                if (popped.right != null) {
-                    queue.add(popped.right);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        TreeNode t1Node1 = new TreeNode(1);
-        TreeNode t1Node3 = new TreeNode(3);
-        TreeNode t1Node2 = new TreeNode(2, t1Node1, t1Node3);
-        TreeNode t1Node6 = new TreeNode(6);
-        TreeNode t1Node9 = new TreeNode(9);
-        TreeNode t1Node7 = new TreeNode(7, t1Node6, t1Node9);
-        TreeNode t1root = new TreeNode(4, t1Node2, t1Node7);
-        printTreeNode(t1root);
-        System.out.print(" -> ");
-        printTreeNode(invertTree(t1root)); // [4,2,7,1,3,6,9] -> [4,7,2,9,6,3,1]
+        TreeNode p1Root = buildTree(Arrays.asList(4,2,7,1,3,6,9));
+        printLevelOrder(p1Root);
+        System.out.print("->");
+        printLevelOrder(invertTree(p1Root)); // [4,2,7,1,3,6,9] -> [4,7,2,9,6,3,1]
 
         System.out.println();
 
-        TreeNode t2Node1 = new TreeNode(1);
-        TreeNode t2Node3 = new TreeNode(3);
-        TreeNode t2root = new TreeNode(2, t2Node1, t2Node3);
-        printTreeNode(t2root);
-        System.out.print(" -> ");
-        printTreeNode(invertTree(t2root)); // [2,1,3] -> [2,3,1]
+        TreeNode p2Root = buildTree(Arrays.asList(2,1,3));
+        printLevelOrder(p2Root);
+        System.out.print("->");
+        printLevelOrder(invertTree(p2Root)); // [2,1,3] -> [2,3,1]
 
         System.out.println();
 
-        TreeNode t3Node2 = new TreeNode(2);
-        TreeNode t3root = new TreeNode(1, t3Node2, null);
-        printTreeNode(t3root);
-        System.out.print(" -> ");
-        printTreeNode(invertTree(t3root)); // [1,2] -> [1,null,2] ** The null is not printed
+        TreeNode p3Root = buildTree(Arrays.asList(1,2));
+        printLevelOrder(p3Root);
+        System.out.print("->");
+        printLevelOrder(invertTree(p3Root)); // [1,2] -> [1,null,2]
 
         System.out.println();
-        printTreeNode(null);
-        System.out.print(" -> ");
-        printTreeNode(invertTree(null)); // [] -> []
+        printLevelOrder(invertTree(null)); // [] -> []
     }
 }
