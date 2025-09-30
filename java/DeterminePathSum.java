c8514b3a10e13d0b730de010e751b9f97e9bbb43
mai-thao <mthaominn@gmail.com>
2025-09-29 21:54:09 -0500

    Use Config buildTree fxn for Path Sum

--- java/DeterminePathSum.java
+++ java/DeterminePathSum.java
@@ -1,6 +1,9 @@
 import configs.Config.*;
 
+import java.util.Arrays;
 import java.util.Stack;
+
+import static configs.Config.buildTree;
 
 /**
  * Given the root of a binary tree and an integer `targetSum`, return true if the tree has a root-to-leaf
@@ -53,41 +56,32 @@ public class DeterminePathSum {
     public static void main(String[] args) {
         System.out.println(hasPathSum(null, 0)); // [], 0 -> false
         System.out.println(hasPathSumRecursive(null, 0));
+
+        System.out.println();
+
         System.out.println(hasPathSum(new TreeNode(1), 1)); // [1], 1 = true
         System.out.println(hasPathSumRecursive(new TreeNode(1), 1));
+
+        System.out.println();
+
         System.out.println(hasPathSum(new TreeNode(1), 2)); // [1], 2 = false
         System.out.println(hasPathSumRecursive(new TreeNode(1), 2));
 
         System.out.println();
 
-        TreeNode t1Node7 = new TreeNode(7);
-        TreeNode t1Node2 = new TreeNode(2);
-        TreeNode t1Node11 = new TreeNode(11, t1Node7, t1Node2);
-        TreeNode t1NodeLeft4 = new TreeNode(4, t1Node11, null);
-        TreeNode t1Node1 = new TreeNode(1);
-        TreeNode t1Node13 = new TreeNode(13);
-        TreeNode t1NodeRight4 = new TreeNode(4, null, t1Node1);
-        TreeNode t1Node8 = new TreeNode(8, t1Node13, t1NodeRight4);
-        TreeNode t1Root = new TreeNode(5, t1NodeLeft4, t1Node8);
+        TreeNode t1Root = buildTree(Arrays.asList(5,4,8,11,null,13,4,7,2,null,null,null,1));
         System.out.println(hasPathSum(t1Root, 22)); // [5,4,8,11,null,13,4,7,2,null,null,null,1], 22 -> true
         System.out.println(hasPathSumRecursive(t1Root, 22));
 
         System.out.println();
 
-        TreeNode t2Node2 = new TreeNode(2);
-        TreeNode t2Node3 = new TreeNode(3);
-        TreeNode t2Root = new TreeNode(1, t2Node2, t2Node3);
+        TreeNode t2Root = buildTree(Arrays.asList(1,2,3));
         System.out.println(hasPathSum(t2Root, 5)); // [1,2,3], 5 -> false
         System.out.println(hasPathSumRecursive(t2Root, 5));
 
         System.out.println();
 
-        TreeNode t3Node1 = new TreeNode(1);
-        TreeNode t3Node3 = new TreeNode(3);
-        TreeNode t3NodeLeftNeg2 = new TreeNode(-2, t3Node1, t3Node3);
-        TreeNode t3NodeRightNeg2 = new TreeNode(-2);
-        TreeNode t3NodeNeg3 = new TreeNode(-3, t3NodeLeftNeg2, null);
-        TreeNode t3Root = new TreeNode(1, t3NodeRightNeg2, t3NodeNeg3);
+        TreeNode t3Root = buildTree(Arrays.asList(1,-2,-3,1,3,-2));
         System.out.println(hasPathSum(t3Root, 3)); // [1,-2,-3,1,3,-2], 3 -> false
         System.out.println(hasPathSumRecursive(t3Root, 3));
     }
