772534e742d0b1830e21ab65faaf89dec3744db5
mai-thao <mthaominn@gmail.com>
2025-10-05 12:39:52 -0500

    Add problem description to Java AddTwoIntegers solution

--- java/AddTwoIntegers.java
+++ java/AddTwoIntegers.java
@@ -1,3 +1,9 @@
+/**
+ * Given two integers num1 and num2, return the sum of the two integers.
+ *
+ * Constraints:
+ *  -100 <= num1, num2 <= 100
+ */
 public class AddTwoIntegers {
     public static int sum(int num1, int num2) {
         return num1 + num2;
